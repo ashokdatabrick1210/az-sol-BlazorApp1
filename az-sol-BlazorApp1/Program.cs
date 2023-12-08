2d845ee22f6a6321cdbb289f28428110eab6a7c0
aharibhaughanwat <140369709+aharibhaughanwat@users.noreply.github.com>
2023-12-08 17:17:58 +0530

    changes

--- az-sol-BlazorApp1/Program.cs
+++ az-sol-BlazorApp1/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var app = builder.Build();
 
